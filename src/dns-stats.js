190cf05d2bfcd68e732bab17d449a2d8581205c7
Alexey Yakauleu MBP <yakovlev@yakovlevdev.com>
2025-01-06 12:15:17 +0300

    feat: dns stats done!

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,9 +22,24 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+  const DNSStats = {}
+
+  domains.forEach(domain => {
+    const split = domain.split('.').reverse();
+    let current = '';
+
+    split.forEach((domain) => {
+      current = `${current}.${domain}`;
+      if (DNSStats[current]) {
+        DNSStats[current] += 1;
+      } else {
+        DNSStats[current] = 1;
+      }
+    })
+  })
+
+  return DNSStats;
 }
 
 module.exports = {
