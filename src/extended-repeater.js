666584ee7e97601596ad5599c32716bff5ca1287
Alexey Yakauleu MBP <yakovlev@yakovlevdev.com>
2025-01-07 01:24:59 +0300

    feat: extended repeater done!

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -2,22 +2,31 @@ const { NotImplementedError } = require('../extensions/index.js');
 
 /**
  * Create a repeating string based on the given parameters
- *  
+ *
  * @param {String} str string to repeat
- * @param {Object} options options object 
+ * @param {Object} options options object
  * @return {String} repeating string
- * 
+ *
  *
  * @example
- * 
- * repeater('STRING', { repeatTimes: 3, separator: '**', 
+ *
+ * repeater('STRING', { repeatTimes: 3, separator: '**',
  * addition: 'PLUS', additionRepeatTimes: 3, additionSeparator: '00' })
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  str = String(str);
+  const addition = options.hasOwnProperty('addition') ? String(options.addition) : '';
+
+  const repeatTimes = options.repeatTimes || 1;
+  const separator = options.separator || '+';
+  const additionRepeatTimes = options.additionRepeatTimes || 1;
+  const additionSeparator = options.additionSeparator || '|';
+
+  const additionPart = new Array(additionRepeatTimes).fill(addition).join(additionSeparator);
+
+  return new Array(repeatTimes).fill(str + additionPart).join(separator);
 }
 
 module.exports = {
